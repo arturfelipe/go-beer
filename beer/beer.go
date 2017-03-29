4c287c1f48f6d7056168d444a8a749ed8793140f
Artur Sousa <artur.sousa@corp.globo.com>
2017-03-29 18:55:37 -0300

    Add a Beer struct

--- beer/beer.go
+++ beer/beer.go
@@ -1,1 +1,9 @@
 package beer
+
+// Beer model
+type Beer struct {
+	Name        string
+	Description string
+	Style       string
+	Abv         int
+}
--- beer/beer_test.go
+++ beer/beer_test.go
@@ -1,1 +1,28 @@
 package beer
+
+import (
+	"testing"
+
+	check "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+type BeerSuite struct {
+	beer *Beer
+}
+
+var _ = check.Suite(&BeerSuite{})
+
+func (s *BeerSuite) SetUpSuite(c *check.C) {
+	s.beer = &Beer{}
+}
+
+func (s *BeerSuite) TestBeerData(c *check.C) {
+	c.Check(s.beer.Name, check.Equals, "")
+	c.Check(s.beer.Description, check.Equals, "")
+	c.Check(s.beer.Style, check.Equals, "")
+	c.Check(s.beer.Abv, check.Equals, 0)
+}
